bb6707015ca470084ac267497eeec7b8969cff8a
IskarTommy <iskardeishini117@gmail.com>
2026-09-13 01:21:27 +0000

    feat: scaffold initial web and api applications with shared api package

--- packages/api/src/entry.ts
+++ packages/api/src/entry.ts
@@ -1,3 +1,2 @@
-export { Link } from './links/entities/link.entity';
-export { CreateLinkDto } from './links/dto/create-link.dto';
-export { UpdateLinkDto } from './links/dto/update-link.dto';
+// Shared type definitions for The Grimoire
+// We'll add our manga and library types here
